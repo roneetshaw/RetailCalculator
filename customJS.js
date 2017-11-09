c6a81ca9aa02d14d8b1d2a5f845196757c610abe
roneetshaw <roneet.kumar.shaw@gmail.com>
2017-11-09 18:16:10 +0530

    updates

--- customJS.js
+++ customJS.js
@@ -31,6 +31,15 @@ $( document ).ready(function() {
 		}
 	});
 
+	$('#radioBtnBHK a').on('click', function(){
+    var sel = $(this).data('title');
+		var tog = $(this).data('toggle');
+		$('#'+tog).prop('value', sel);
+		
+		$('a[data-toggle="'+tog+'"]').not('[data-title="'+sel+'"]').removeClass('active').addClass('notActive');
+		$('a[data-toggle="'+tog+'"][data-title="'+sel+'"]').removeClass('notActive').addClass('active');
+	})
+	
 	$('#radioBtnHouse a').on('click', function(){
     var sel = $(this).data('title');
 		var tog = $(this).data('toggle');
